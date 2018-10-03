8f95a00a640c3010b5b8743f921594ac9248707a
Vivian Breda <vivianbreda@gmail.com>
2018-10-03 17:14:28 +1300

    Fix a bug that prevent .txt to be displayed.

--- MediaLibraryManager/MediaLibraryManager/Custom View Controllers/DocumentViewController.swift
+++ MediaLibraryManager/MediaLibraryManager/Custom View Controllers/DocumentViewController.swift
@@ -10,41 +10,42 @@ import Cocoa
 import Quartz
 
 /**
-The View Controller for our Document media.
-Sets up and displays the media.
-*/
+ The View Controller for our Document media.
+ Sets up and displays the media.
+ */
 class DocumentViewController: NSViewController {
     
     var fileToOpen : MMFile = File(filename: "default")
     var fullpath : String = ""
-	
+    
     @IBOutlet weak var documentView: NSScrollView!
     @IBOutlet var textView: NSTextView!
     @IBOutlet weak var pdfView: PDFView!
     @IBOutlet weak var failIndicator: NSProgressIndicator!
     @IBOutlet weak var failMessage: NSTextField!
-	
-	/**
-	Initialises the View Controller with a specific file as the start point.
-	Sets the fullpath.
-	*/
-	convenience init(file: MMFile) {
-		self.init()
-		fileToOpen = file
-		fullpath = file.path+"/"+file.filename
-	}
-
-	/**
-	Called when the view loaded successfully.
-	Shows the media file.
-	*/
+    
+    /**
+     Initialises the View Controller with a specific file as the start point.
+     Sets the fullpath.
+     */
+    convenience init(file: MMFile) {
+        self.init()
+        fileToOpen = file
+        fullpath = file.path+"/"+file.filename
+    }
+    
+    /**
+     Called when the view loaded successfully.
+     Shows the media file.
+     */
     override func viewDidLoad() {
         failIndicator.isHidden = true
         failMessage.isHidden = true
+        pdfView.isHidden = true
         super.viewDidLoad()
         showText()
     }
-	
+    
     func showText() {
         
         let filepath : String = NSString(string: fullpath).expandingTildeInPath
@@ -52,19 +53,18 @@ class DocumentViewController: NSViewController {
         let ext : String = separateExtension[1]
         
         if(ext == "txt") {
-			do {
-				let contents = try String(contentsOfFile: filepath)
-				textView.string = contents;
-				textView.isEditable = false ;
-			} catch {}
+            do {
+                let contents = try String(contentsOfFile: filepath)
+                textView.string = contents;
+                textView.isEditable = false ;
+            } catch {}
         } else if (ext == "pdf") {
-            //pdfView.image = NSImage(contentsOfFile: filepath)
-                let url = URL(fileURLWithPath: filepath)
-                if let pdfDocument = PDFDocument(url: url) {
-                    pdfView.displayMode = .singlePageContinuous
-                    pdfView.autoScales = true
-                    // pdfView.displayDirection = .horizontal
-                    pdfView.document = pdfDocument
+            pdfView.isHidden = false
+            let url = URL(fileURLWithPath: filepath)
+            if let pdfDocument = PDFDocument(url: url) {
+                pdfView.displayMode = .singlePageContinuous
+                pdfView.autoScales = true
+                pdfView.document = pdfDocument
             }
         } else {
             failIndicator.isHidden = false
